02e91e0ce22f25ee5d34843404eb2d484f0c1df0
Marcdonaldtec <marcdonaldtech40@gmail.com>
2024-02-19 13:01:21 -0500

    tenth commit

--- marc_platform/settings.py
+++ marc_platform/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-9)%zg00o*(7+%kakp-u!c7kb0bgnn*3m-qw!g&u3q0e+*4eu$)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 INSTALLED_APPS = [
