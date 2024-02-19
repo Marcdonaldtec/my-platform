edd2e563bc2af0eacc01a57c57ece7d5310d276f
Marcdonaldtec <marcdonaldtech40@gmail.com>
2024-02-18 23:23:48 -0500

    The third commit

--- marc_platform/settings.py
+++ marc_platform/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'marc_platform.urls'
@@ -123,6 +124,7 @@ USE_TZ = True
 import os
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
