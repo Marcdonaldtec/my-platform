b8e56e148b4d0c0b02d49699a3c577cf69976fef
Marcdonaldtec <marcdonaldtech40@gmail.com>
2024-03-03 22:22:37 -0500

    Added collectstaticfiles

--- marc_platform/settings.py
+++ marc_platform/settings.py
@@ -126,8 +126,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-MEDIA_ROOT = BASE_DIR / "media"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR / "static")
+    ]
+MEDIA_ROOT = os.path.join(BASE_DIR / "media")
 
 # Ajoutez ces configurations si vous souhaitez activer HSTS
 SECURE_HSTS_SECONDS = 31536000
