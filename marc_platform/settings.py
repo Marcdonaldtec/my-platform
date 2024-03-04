33d4c052e442fe910ef7ccd859467d553c3c3f95
Marcdonaldtec <marcdonaldtech40@gmail.com>
2024-03-04 16:52:29 -0500

    Made the interface more user-friendly

--- marc_platform/settings.py
+++ marc_platform/settings.py
@@ -29,11 +29,13 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = False
-# DEBUG = os.environ.get("DEBUG", "False").lower()==True 
+DEBUG = os.environ.get("DEBUG", "False").lower()==True 
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 # ALLOWED_HOSTS = ['.onrender.com']
+
+
 
 
 INSTALLED_APPS = [
